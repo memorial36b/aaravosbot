8b37dd66a2629577ce37f693a89a3dfded1ef2ea
asakala <asakala@ncsu.edu>
2019-02-27 09:57:58 -0500

    Add ping crystal

---
+++ src/main/ping.rb
@@ -0,0 +1,30 @@
+# Crystal: Ping
+
+
+# A simple ping command for the bot, with response time measurement.
+module Bot::Ping
+  # These two lines can be removed as needed (i.e. if your crystal does not contain any commands or event handlers)
+  extend Discordrb::Commands::CommandContainer
+  extend Discordrb::EventContainer
+
+  # Ping command
+  command :ping do |event|
+    before = Time.now
+    msg = event.respond '**PongChamp**'
+    after = Time.now
+    msg.edit "**PongChamp** | #{((after - before) * 1000).round}ms response time"
+  end
+
+  # Help command info
+  module HelpInfo
+    extends HelpCommand
+
+    # +ping
+    command_info(
+        name: :ping,
+        blurb: 'Pings the bot.',
+        permission: :user,
+        info: ['Pings the bot, showing the response time.']
+    )
+  end
+end
